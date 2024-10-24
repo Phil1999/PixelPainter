8579e71169a39c42ff8a99df7da315a71a4cfdf6
Tim Hsieh <timhsieh.erc@gmail.com>
2024-10-23 19:34:21 -0700

    Utilized Managers to handle grid, picture bank, HUD.

---
+++ PixelPainter/Helpers/BankManager.swift
@@ -0,0 +1,8 @@
+//
+//  BankManager.swift
+//  PixelPainter
+//
+//  Created by Tim Hsieh on 10/23/24.
+//
+
+import Foundation
---
+++ PixelPainter/Helpers/HUDManager.swift
@@ -0,0 +1,8 @@
+//
+//  HUDManager.swift
+//  PixelPainter
+//
+//  Created by Tim Hsieh on 10/23/24.
+//
+
+import Foundation
