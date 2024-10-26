aa5046f89ee7a1df4b47c0899564cbe49f56e9f2
Philip Lee <philiplee1999@gmail.com>
2024-10-25 22:42:21 -0700

    added big countdown text when 5 sec remaining

--- PixelPainter/Helpers/HUDManager.swift
+++ PixelPainter/Helpers/HUDManager.swift
@@ -42,32 +42,61 @@ class HUDManager {
         guard let gameScene = gameScene else { return }
         
         gameScene.context.gameInfo.timeRemaining -= 1
+        let timeRemaining = gameScene.context.gameInfo.timeRemaining
+        
+        // Update regular timer label
         if let timerLabel = gameScene.childNode(withName: "//timerLabel") as? SKLabelNode {
-            timerLabel.text = "Time: \(Int(gameScene.context.gameInfo.timeRemaining))"
-            
-            let timeRemaining = gameScene.context.gameInfo.timeRemaining
+            timerLabel.text = "Time: \(Int(timeRemaining))"
             
             if timeRemaining > 0 && timeRemaining <= 5 {
-                timerLabel.fontColor = .red
-            } else {
-                timerLabel.fontColor = .white
+                showBigCountdown(time: Int(timeRemaining))
             }
         }
         
-        // Time warning flash before game over.
-        let timeRemaining = gameScene.context.gameInfo.timeRemaining
-        
+        // Time warning flash before game over
         if timeRemaining > 0 && timeRemaining <= 5 {
             if let playState = gameScene.context.stateMachine?.currentState as? PlayState {
                 playState.effectManager.flashScreen(color: .red, alpha: 0.3)
             }
         }
         
-        
         // Game over check
-        if gameScene.context.gameInfo.timeRemaining <= 0 {
+        if timeRemaining <= 0 {
             gameScene.context.stateMachine?.enter(GameOverState.self)
         }
+    }
+
+    private func showBigCountdown(time: Int) {
+        guard let gameScene = gameScene else { return }
+        
+        let countdownLabel = SKLabelNode(text: "\(time)")
+        countdownLabel.fontName = "PPNeueMontreal-Bold"
+        countdownLabel.fontSize = 100
+        countdownLabel.fontColor = .red
+        
+        let gridTopY = (gameScene.size.height / 2 + 50) + (gameScene.context.layoutInfo.gridSize.height / 2)
+        countdownLabel.position = CGPoint(
+            x: gameScene.size.width / 2,
+            y: gridTopY + 50
+        )
+
+        countdownLabel.zPosition = 9999
+        gameScene.addChild(countdownLabel)
+        
+        // Animation sequence
+        let scaleUp = SKAction.scale(to: 1.5, duration: 0.2)
+        let scaleDown = SKAction.scale(to: 1.0, duration: 0.2)
+        let fadeOut = SKAction.fadeOut(withDuration: 0.2)
+        let remove = SKAction.removeFromParent()
+        
+        let sequence = SKAction.sequence([
+            scaleUp,
+            scaleDown,
+            fadeOut,
+            remove
+        ])
+        
+        countdownLabel.run(sequence)
     }
     
     func updateScore() {
